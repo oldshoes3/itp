f67e19bb5c6430d7907be4e01c34311aa0ae665d
YuseokSeol <wrsiryouyou@gmail.com>
2024-03-03 22:50:26 -0500

    I think I did it without two for loops

--- 04Pyramid/SEOL_Pyramid.py
+++ 04Pyramid/SEOL_Pyramid.py
@@ -1,1 +1,6 @@
-# well
+stacks = int(input('Kindly enter a number between 1 and 8: '))
+if stacks < 1 or stacks > 8:
+    print("Seriously what's wrong with you")
+else:
+    for i in range(1, stacks + 1):
+        print(' ' * (stacks - i), '#' * i, sep='')
